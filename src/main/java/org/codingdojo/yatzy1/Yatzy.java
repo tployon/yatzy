94f845b96b7e3d8d5575dcc90a0ecefb4ff6deec
thomas ployon <thomas.ployon@gmail.com>
2024-04-11 09:40:09 +0200

    Introduce Yatzy object in chance.

--- src/main/java/org/codingdojo/yatzy1/Yatzy.java
+++ src/main/java/org/codingdojo/yatzy1/Yatzy.java
@@ -6,13 +6,8 @@ public class Yatzy {
 
     public static int chance(int d1, int d2, int d3, int d4, int d5)
     {
-        int total = 0;
-        total += d1;
-        total += d2;
-        total += d3;
-        total += d4;
-        total += d5;
-        return total;
+        Yatzy yatzy = new Yatzy(List.of(d1, d2, d3, d4, d5));
+        return yatzy.dices.stream().reduce(0, Integer::sum);
     }
 
     public static int yatzy(int... dice)
