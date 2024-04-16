76923b5f7c278083b864f6fa4866c952ce32a608
thomas ployon <thomas.ployon@gmail.com>
2024-04-16 11:49:59 +0200

    Refactor full house.

--- src/main/java/org/codingdojo/yatzy1/Yatzy.java
+++ src/main/java/org/codingdojo/yatzy1/Yatzy.java
@@ -149,38 +149,11 @@ public class Yatzy {
         return sum();
     }
 
-    public static int fullHouse(int d1, int d2, int d3, int d4, int d5) {
-        int[] tallies;
-        boolean _2 = false;
-        int i;
-        int _2_at = 0;
-        boolean _3 = false;
-        int _3_at = 0;
-
-
-        tallies = new int[6];
-        tallies[d1 - 1] += 1;
-        tallies[d2 - 1] += 1;
-        tallies[d3 - 1] += 1;
-        tallies[d4 - 1] += 1;
-        tallies[d5 - 1] += 1;
-
-        for (i = 0; i != 6; i += 1)
-            if (tallies[i] == 2) {
-                _2 = true;
-                _2_at = i + 1;
-            }
-
-        for (i = 0; i != 6; i += 1)
-            if (tallies[i] == 3) {
-                _3 = true;
-                _3_at = i + 1;
-            }
-
-        if (_2 && _3)
-            return _2_at * 2 + _3_at * 3;
-        else
-            return 0;
+    public static int fullHouse(Yatzy yatzy) {
+        Map<Integer, Long> frequencies = yatzy.frequencies();
+        if(frequencies.size() !=2 || !frequencies.containsValue(3L))
+            return ZERO;
+        return yatzy.sum();
     }
 }
 
--- src/test/java/org/codingdojo/YatzyTest.java
+++ src/test/java/org/codingdojo/YatzyTest.java
@@ -118,7 +118,7 @@ public class YatzyTest {
 
     @Test
     public void fullHouse() {
-        assertEquals(18, Yatzy.fullHouse(6,2,2,2,6));
-        assertEquals(0, Yatzy.fullHouse(2,3,4,5,6));
+        assertEquals(18, Yatzy.fullHouse(new Yatzy(List.of(6, 2, 2, 2, 6))));
+        assertEquals(0, Yatzy.fullHouse(new Yatzy(List.of(2, 3, 4, 5, 6))));
     }
 }
